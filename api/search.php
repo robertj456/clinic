4abaaa86dcddc767ac5bdac638e107c8315222af
unknown <1237488@2F14-28.ad.dawsoncollege.qc.ca>
2014-11-26 17:00:42 -0500

    the

--- api/search.php
+++ api/search.php
@@ -1,33 +1,37 @@
 <?php
 
 include 'database.php';
-$visit=$_GET['visitID'];
-$visit=1;
+
+
+if(isset($_GET['visitID'])&&is_numeric($_GET['visitID']))
+{
+	$visit=$_GET['visitID'];
 
 $sql = "SELECT * from visit where Visit_ID = ?";
 $visit = excuteQuery($sql, [$visit]);
 
+$sql = "SELECT * from patient where PATIENT_ID = ?";
+$patient = excuteQuery($sql, [$visit[0]['PATIENT_ID']]);
+
+// do proper questuff
+$sql = "SELECT * from queue where queue_name = ?";
+$code = excuteQuery($sql, ["code ".$visit[0]['CODE']]);
 
 
 
-$str='{';
-$str =$str."\"visit\":{";
-foreach($visit[0] as $key => $value){
-$str =$str."\"$key\":\"$value\",";
+
+$json =[$visit[0],$patient[0], $code[0]];
+
+
+echo json_encode($json);
+
 }
-$str=substr($str, 0, -1);
-$str =$str.'}';
 
+else{
+// if improper input
+echo 'ERROR';
 
-
-$str = $str.'}';
-echo $str;
-
-$sql = "SELECT * from patient where paitent_ID = ?";
-$patient = excuteQuery($sql, [$visit]);
-
-
-
+}
 
 
 ?>
